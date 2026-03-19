15e6e385e813df743920628f83dfd520c8be3af5
kdy-aws <donnyya1031@gmail.com>
2026-03-19 16:15:11 +0900

    frontend cors permit

---
+++ member/src/main/java/com/example/ordersystem/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.ordersystem.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://awskdy.click")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
